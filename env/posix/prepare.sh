96d8e6c1ef56e54c7948f3068db43e6efa954a03
Christoph Rupp <chris@crupp.de>
2012-07-24 13:56:07 +0200

    release 2.0.4

--- env/posix/prepare.sh
+++ env/posix/prepare.sh
@@ -3,7 +3,7 @@
 CUR=`pwd`
 HAM=/home/chris/prj/hamsterdb_master/
 HAM=/home/chris/prj/hamsterdb/
-#HAM=/home/chris/tmp/hamsterdb
+HAM=/home/chris/tmp/hamsterdb-2.0.4
 
 # update the header files
 # cp $HAM/include/ham/*.h ../common/ham/
