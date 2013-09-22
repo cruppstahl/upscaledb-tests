3d42c80630678a93c1296fdc738069d98a0c2d62
Christoph Rupp <chris@crupp.de>
2013-09-22 16:15:54 +0200

    Monster.pl is a replacement for monster.sh and the valgrind tests

--- env/posix/monster.pl
+++ env/posix/monster.pl
@@ -8,12 +8,20 @@ my $errors = 0;
 my $total = 0;
 
 sub run_single_test {
+  my $valgrind = shift;
   my $file = shift;
   my $params = shift;
 
   open(FH, ">>monster.txt") or die "Cannot open monster.txt for writing";
   print "[START] ./test $params $file\n";
-  my $output = `./test $params $file`;
+
+  my $output;
+  if ($valgrind) {
+    $output = `valgrind --tool=memcheck --suppressions=valgrind.suppress ./test $params $file`;
+  }
+  else {
+    $output = `./test $params --use-berkeleydb=true --reopen=true $file`;
+  }
   print "[STOP]  ./test $params $file\n";
   print FH "./test $params $file\n";
   print FH $output;
@@ -25,15 +33,27 @@ sub run_single_test {
 
 sub run_directory {
   my $dryrun = shift;
+  my $valgrind = shift;
   my $maxdir = shift;
   my $options = shift;
 
   print "[CONFIGURATION] $options\n";
-  for (my $i = 1; $i <= $maxdir; $i++) {
-    my @files = <../../testfiles/$i/*.tst>;
+
+  if ($valgrind) {
+    my @files = ('1/45.tst', '1/220.tst', '1/ext_020.tst', '2/ext_060.tst',
+        '2/blb-001.tst');
     foreach my $file (@files) {
-      run_single_test($file, $options);
+      run_single_test($valgrind, "../../testfiles/$file", $options);
       return if $dryrun;
+    }
+  }
+  else {
+    for (my $i = 1; $i <= $maxdir; $i++) {
+      my @files = <../../testfiles/$i/*.tst>;
+      foreach my $file (@files) {
+        run_single_test($valgrind, $file, $options);
+        return if $dryrun;
+      }
     }
   }
 }
@@ -44,10 +64,32 @@ sub run {
 
   `rm -f *.db* monster.txt`;
 
-  open FILE, 'monster.lst' or die "Couldn't open file monster.lst: $!";
+  if ($c->options->{'dir'}) {
+    run_directory($dryrun, 0, $c->options->{'dir'}, $c->options->{'options'});
+  }
+  else {
+    open FILE, 'monster.lst' or die "Couldn't open file monster.lst: $!";
+    foreach (<FILE>) {
+      if (/(\d), \"(.*?)\"$/) {
+        run_directory($dryrun, 0, $1, $2);
+      }
+    }
+  }
+
+  print "\n$errors of $total tests failed\n";
+  return '';
+}
+
+sub valgrind {
+  my $c = shift;
+  my $dryrun = $c->options->{'dryrun'};
+
+  `rm -f *.db* valgrind.txt`;
+
+  open FILE, 'valgrind.lst' or die "Couldn't open file valgrind.lst: $!";
   foreach (<FILE>) {
     if (/(\d), \"(.*?)\"$/) {
-      run_directory($dryrun, $1, $2);
+      run_directory($dryrun, 1, $1, $2);
     }
   }
 
@@ -58,7 +100,9 @@ sub run {
 sub setup {
   my $c = shift;
   $c->register_commands( {
-        run => 'runs the monster tests (can take a day or two...)'
+        run => "runs the monster tests (can take a day or two...;\n" .
+                "\t\targuments: --dir=1|2|3|4; --options=OPTIONS)",
+        valgrind => 'runs assorted tests in valgrind' 
     });
 }
 
